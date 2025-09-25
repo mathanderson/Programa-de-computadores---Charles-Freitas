5063236a7a77d142246392b2003927091365903c
mathanderson <amatheus1111@gmail.com>
2025-09-24 21:46:54 -0300

    Exercícios - 24.09.2025

--- Aula de exercícios/Exercício - Trapézio - 24.09.2025.py
+++ Aula de exercícios/Exercício - Trapézio - 24.09.2025.py
@@ -1,4 +1,4 @@
-print('Vamos calcular a área de um trapezio')
+print('Vamos calcular a área de um trapézio')
 
 baseMaior = float(input('Informe o valor da Base Maior: '))
 
@@ -8,4 +8,4 @@ altura = float(input('Informe o valor da Altura: '))
 
 area = (baseMaior + baseMenor) * altura / 2
 
-print(f'A área do trapezio é {area:.1f}') 
+print(f'A área do trapézio é {area:.1f}')
