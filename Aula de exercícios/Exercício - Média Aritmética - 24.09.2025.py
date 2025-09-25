9b05abd35e35121c53a0578a9b56b3f1eb4ddfd6
mathanderson <amatheus1111@gmail.com>
2025-09-24 21:52:08 -0300

    Exercício - Média aritmética

---
+++ Aula de exercícios/Exercício - Média Aritmética - 24.09.2025.py
@@ -0,0 +1,10 @@
+print ('Desenvolva um programa em Pyton que solicite ao usuário dois valores inteiros e em seguida calcule a média aritmética entre os dois')
+
+
+valorA = int(input('Informe o primeiro valor: '))
+
+valorB = int(input('Informe o segundo valor: '))
+
+media = (valorA + valorB) / 2
+
+print(f'A média entre {valorA} e {valorB} é {media}')
